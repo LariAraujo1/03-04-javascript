8ab61ffdc3ecf4e8283843e1ca47ffb285a55e9c
LARISSA TAMIRES DE ARAUJO NASCIMENTO <larissa.tanascimento@senacsp.edu.br>
2024-04-03 16:28:14 -0300

    att

--- DOM/js/script.js
+++ DOM/js/script.js
@@ -1,1 +1,32 @@
 console.log(document.body)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[1])
+console.log(document.body.childNodes[1].childNodes[1].textContent)
+
+// 2- selecionando por tag
+const listItens = document.getElementsByTagName("li")
+console.log(listItens)
+
+//selecionando por id
+const title = document.getElementById("title")
+console.log(title)
+
+const products= document.getElementsByClassName("product");
+console.log(products)
+
+const productsQuery=document.querySelectorAll(".product")
+console.log(products)
+
+const mainContainer=document.querySelector("#main-container");
+console.log(main-container);
+
+const p = document.createElement("p");
+const header = title.parentElement;
+console.log(header) 
+header.insertBefore(p, title);
+console.log(header) 
+
+
+const navLinks = document.querySelector("nav ul");
+const li = document.createElement("li");
+navLinks.appendChild(li);
